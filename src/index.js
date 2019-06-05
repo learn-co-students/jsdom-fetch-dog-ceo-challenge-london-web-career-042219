420b5e3c18a8c9cd12e8b7b45dcf75050844a601
miaoshan <minnieshan@gmail.com>
2019-06-05 08:55:09 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+const dogImage = (imgUrl) => {
+  const dogWrapper = document.createElement('div');
+  dogWrapper.className = 'dog-wrapper';
+
+  const html = `<img src="${imgUrl}" class="dog-image" alt="A cute dog">`;
+  dogWrapper.innerHTML = html;
+
+  return dogWrapper;
+};
+
+const addDogImages = (urls) => {
+  const dogImageContainer = document.querySelector('#dog-image-container');
+  urls.forEach((url) => {
+    dogImageContainer.appendChild(dogImage(url));
+  });
+};
+
+const fetchDogImages = () => fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(response => response.json());
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchDogImages()
+    .then(json => addDogImages(json.message));
+});
+
+console.log('%c HI', 'color: firebrick');
