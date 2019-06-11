ce6eccb2f99f8e2419b72c6409e2a1a473e1e335
github username <github email address>
2019-06-11 10:24:23 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ const dropDown = document.querySelector("#breed-dropdown");
 const breedElement = document.querySelector("#dog-breeds");
 const container = document.querySelector("#dog-image-container");
 const ulBreed = document.querySelector("#dog-breeds")
+let localBreeds = {}
+
+
+function transformIntoArray (breedsObject) {
+	const result = []
+	for (const key in breedsObject) {
+        result.push({ name: key, subBreeds: breedsObject[key] })
+	}
+	return result
+}
 
 function fetchDogs() {
     fetch(imgUrl)
@@ -29,33 +39,43 @@ renderDogs = (dogs) => {
 function fetchBreeds() {
     return fetch(breedUrl)
         .then(response => response.json())
-        .then(breedObject => renderBreeds(breedObject));
+        .then(breedObject => {
+            localBreeds = transformIntoArray(breedObject.message)
+            renderBreeds(localBreeds)
+        });
 }
 
 function renderBreeds(breeds) {
+    ulBreed.innerHTML = ""
     //first I need to select the area by ID of where the dog images will be rendered. 
-    for (const key in breeds["message"]) { 
+    for (const breed of breeds) { 
     // You an look through the object by saying const key of (object)  in this case we need to look through the object(message) to go one level deeper.
         const liBreed = document.createElement("li")
-        liBreed.innerText = `${key}`
+        liBreed.innerText = `${breed.name}`
         ulBreed.append(liBreed) 
     }
 
     breedElement.addEventListener('click', function(event) {
         event.target.style.color = "#FF4500";
     })
-   
-    // dropDown.addEventListener("change", function(event) {
-    //     //show only the 
-    //     breedElement that starts with event.target.value. 
-    // })
 }
 
-
+function addDropdownListener () {
+    dropDown.addEventListener("change", function(event) {
+        if (dropDown.value.length == 0) {
+             renderBreeds(localBreeds);
+        } else {
+            const filteredBreeds = localBreeds
+            .filter(breed => breed.name[0] == dropDown.value);
+            renderBreeds(filteredBreeds);
+        }                                
+    })
+}
 
 function init(){
     fetchDogs();
     fetchBreeds();
+    addDropdownListener();
 }
 
 
